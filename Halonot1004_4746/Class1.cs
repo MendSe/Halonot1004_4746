ff2ab6b61bac30deb49ff5bb3f20130f847fa697
S37M <setti@g.jct.ac.il>
2023-03-21 17:19:19 +0200

    Add project files.

---
+++ Halonot1004_4746/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Halonot1004_4746
+{
+    internal class Class1
+    {
+    }
+}
